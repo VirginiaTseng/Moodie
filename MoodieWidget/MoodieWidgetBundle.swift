d92f536f66d6e19767900121be9b6900397f6855
VirginiaTseng <huijuan.zheng@gmail.com>
2025-02-20 21:58:14 -0600

    dayfeb20-2 add widget

---
+++ MoodieWidget/MoodieWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MoodieWidgetBundle.swift
+//  MoodieWidget
+//
+//  Created by Virginia Zheng on 2025-02-20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MoodieWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MoodieWidget()
+    }
+}
